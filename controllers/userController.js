b8723dd4313c52794d4ec43fbb0b17f419d8672d
BoMiao2020 <miaobo201808@gmail.com>
2020-04-23 19:36:40 -0700

    showuser

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,79 +2,89 @@
 /*globals io: false */
 "use strict";
 var mysql = require("mysql");
-var mysqldb = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Mb2047809!!",
-    database: "testmysqldb" // schema name
+var pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "Mb2047809!!",
+  database: "testmysqldb", // schema name
 });
-
+var async = require("async");
 
 exports.showuser = function () {
-    return function (req, res) {
-        /**
-         * 查找数据库 返回所有用户数组 users  [{name:bomiao, email:...},{name:xiaoming, email:..}]
-         */
+  return function (req, res) {
+    /**
+     * 查找数据库 返回所有用户数组 users  [{name:bomiao, email:...},{name:xiaoming, email:..}]
+     */
+    var users = "none";
 
-            res.render('index', {
-                title: 'Show users',
-                user: req.user == undefined ? 'none' : req.user,
-                returnList: users,
-                action: 'showuser',
-                msg: 'none'
-            });
-    };
+    pool.getConnection(function (err, mysqldb) {
+      if (err) throw err;
+
+      var table = "testuser"; // table name
+      var sql = `SELECT * FROM ${table}`;
+      mysqldb.query(sql, function (err, result, fields) {
+        if (err) throw err;
+        users = result;
+        res.render("index", {
+          title: "Show users",
+          user: req.user == undefined ? "none" : req.user,
+          returnList: users,
+          action: "showuser",
+          msg: "none",
+        });
+      });
+      pool.releaseConnection(mysqldb);
+    });
+  };
 };
 
 exports.deleteuser = function () {
-    return function (req, res) {
-        res.render('index', {
-            title: 'Delete user',
-            user: req.user == undefined ? 'none' : req.user,
-            returnList: 'none',
-            action: 'deleteuser',
-            msg: 'none'
-        });
-    };
+  return function (req, res) {
+    res.render("index", {
+      title: "Delete user",
+      user: req.user == undefined ? "none" : req.user,
+      returnList: "none",
+      action: "deleteuser",
+      msg: "none",
+    });
+  };
 };
 
 exports.edituser = function () {
-    return function (req, res) {
-        res.render('index', {
-            title: 'Edit user',
-            user: req.user == undefined ? 'none' : req.user,
-            returnList: 'none',
-            action: 'edituser',
-            msg: 'none'
-        });
-    };
+  return function (req, res) {
+    res.render("index", {
+      title: "Edit user",
+      user: req.user == undefined ? "none" : req.user,
+      returnList: "none",
+      action: "edituser",
+      msg: "none",
+    });
+  };
 };
 
 //user的各种post处理
 exports.deleteuserCRUD = function () {
-    return function (req, res) {
-        var username = req.body.username;
+  return function (req, res) {
+    var username = req.body.username;
 
-        /**
-         * 后端删除该user账号
-         */
+    /**
+     * 后端删除该user账号
+     */
 
-            res.redirect("showuser");
-        };
-    };
+    res.redirect("showuser");
+  };
+};
 
 exports.edituserCRUD = function () {
-    return function (req, res) {
-        var username = req.user.username;
-         var NameNew = req.body.nameNew;
-        var EmailNew = req.body.emailNew;
-        var PhoneNew = req.body.phoneNew;
-        /**
-         * 后端更新该账号
-         */
+  return function (req, res) {
+    var username = req.user.username;
+    var NameNew = req.body.nameNew;
+    var EmailNew = req.body.emailNew;
+    var PhoneNew = req.body.phoneNew;
+    /**
+     * 后端更新该账号
+     */
 
-
-            res.redirect("showuser");
-
-    };
+    res.redirect("showuser");
+  };
 };
